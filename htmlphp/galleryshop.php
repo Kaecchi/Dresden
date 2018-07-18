9de9023c8ac8b3892a884339af78fdbce2548f19
Havystar <Havystar@users.noreply.github.com>
2018-07-18 09:41:38 +0200

    poprawki znowu

--- htmlphp/galleryshop.php
+++ htmlphp/galleryshop.php
@@ -11,8 +11,8 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   <!-- Bootstrap CSS --> 
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+<link href="css/galleryshop.css" rel="stylesheet">
   <link href="css/menufooter.css" rel="stylesheet">
-  <link href="css/galleryshop.css" rel="stylesheet">
   <link rel="stylesheet" href="fontello-aa345661/css/fontello.css">
 
   
